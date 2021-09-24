c933aa1ebf47638303f08877d5f6280db7f9fa45
Robert de Bock <robert@meinit.nl>
2021-09-24 11:39:40 +0200

    Use cloud-init instead.

--- examples/github_runner/main.tf
+++ examples/github_runner/main.tf
@@ -1,13 +1,7 @@
 module "azurerm_virtual_machine" {
-  source   = "../../"
-  commands = <<-EOF
-export runner_version="2.283.1"
-export RUNNER_ALLOW_RUNASROOT="1"
-wget -qO- "https://github.com/actions/runner/releases/download/v$runner_version/actions-runner-linux-x64-$runner_version.tar.gz" | tar xvz -
-./config.sh --url "https://github.com/robertdebock" --token FaKeToKeN --runasservice --unattended --replace
-sudo ./svc.sh install
-sudo ./svc.sh start
-EOF
+  source       = "../../"
+  name         = "githubrunner'
+  cloud_config = "cloud_config"
 }
 
 output "public_ip_address" {
